ea64093a588c6aa6ce0654f6e4e212dea412f033
Jardel Weyrich <jweyrich@gmail.com>
2012-05-13 04:37:07 -0300

    FileSink is now seekable. MemorySink is now RO or RW (still needs a better design). Provide shift operators for IoSink and [io]streams/strings.

--- include/sslpkix/iosink.h
+++ include/sslpkix/iosink.h
@@ -56,6 +56,25 @@ public:
 	virtual const std::string source() const {
 		return _filename;
 	}
+	bool rewind() {
+		return seek(0);
+	}
+	bool seek(long offset) {
+		int ret = BIO_seek(_handle, offset);
+		if (ret == -1) {
+			std::cerr << "Failed to seek to file offset: " << offset << std::endl;
+			return false;
+		}
+		return true;
+	}
+	int tell() {
+		int ret = BIO_tell(_handle);
+		if (ret == -1) {
+			std::cerr << "Failed to tell on file." << std::endl;
+			return -1;
+		}
+		return ret;
+	}
 protected:
 	std::string _filename;
 };
@@ -64,15 +83,25 @@ class MemorySink : public IoSink {
 public:
 	MemorySink() : _buffer(NULL), _size(0) {
 	}
-	virtual bool open(void *buffer, int size) {
+	virtual bool open_ro(void *buffer, int size) {
 		release();
 		_handle = BIO_new_mem_buf(buffer, size);
 		if (_handle == NULL)
-			std::cerr << "Failed to create memory BIO: " << buffer << std::endl;
+			std::cerr << "Failed to create readonly memory BIO: " << buffer << std::endl;
 		_buffer = buffer;
-		_size = size;
+		_size = size == -1 ? strlen(buffer) : size;
 		return _handle != NULL;
 	}
+	virtual bool open_rw() {
+		release();
+		_handle = BIO_new(BIO_s_mem());
+		if (_handle == NULL)
+			std::cerr << "Failed to create readwrite memory BIO." << std::endl;
+		_buffer = NULL;
+		_size = 0;
+		return _handle != NULL;
+	}
+
 	virtual const std::string source() const {
 		return "<MemorySink>";
 	}
@@ -80,5 +109,10 @@ protected:
 	void *_buffer;
 	int _size;
 };
+
+IoSink& operator<<(IoSink& sink, const std::string& str);
+IoSink& operator>>(IoSink& sink, std::string& str);
+std::ostream& operator<<(std::ostream& stream, IoSink& sink);
+std::istream& operator>>(std::istream& stream, IoSink& sink);
 
 } // namespace sslpkix
---
+++ src/iosink.cpp
@@ -0,0 +1,89 @@
+#include "sslpkix/iosink.h"
+
+namespace sslpkix {
+
+IoSink& operator<<(IoSink& sink, const std::string& str) {
+	const size_t total_size = str.length();
+	const char *buf = str.data();
+	const char *current = buf;
+	size_t written_size = 0;
+	int ret = 0;
+
+	do {
+		ret = BIO_write(sink.handle(), current, total_size - written_size);
+		if (ret > 0) {
+			current += ret;
+			written_size += static_cast<size_t>(ret);
+		}
+	} while (ret > 0 && written_size < total_size);
+
+	if (ret == 0 || ret == -1)
+		throw std::ios_base::failure("BIO_write failed");
+	else if (ret == -2)
+		throw std::ios_base::failure("BIO_write is not implemented for this BIO");
+
+	// TODO: Need to check result? 1=success, 0 or -1=failure
+	ret = BIO_flush(sink.handle());
+
+	return sink;
+}
+
+IoSink& operator>>(IoSink& sink, std::string& str) {
+	char buf[1024];
+	int ret = 0;
+
+	do {
+		ret = BIO_read(sink.handle(), buf, sizeof(buf));
+		if (ret > 0)
+			str.append(buf, ret);
+	} while (ret > 0);
+
+	if (ret == -1)
+		throw std::ios_base::failure("BIO_read failed");
+	else if (ret == -2)
+		throw std::ios_base::failure("BIO_read is not implemented for this BIO");
+
+	return sink;
+}
+
+std::ostream& operator<<(std::ostream& stream, IoSink& sink) {
+	char buf[1024];
+	int ret = 0;
+
+	do {
+		ret = BIO_read(sink.handle(), buf, sizeof(buf)-1);
+		if (ret > 0) {
+			buf[ret] = '\0';
+			stream << buf;
+		}
+	} while (ret > 0);
+
+	if (ret == -1)
+		; // EOF?
+	else if (ret == -2)
+		throw std::ios_base::failure("BIO_read is not implemented for this BIO");
+
+	return stream;
+}
+
+std::istream& operator>>(std::istream& stream, IoSink& sink) {
+	char buf[1024];
+	int ret = 0;
+
+	while (!stream.eof()) {
+		stream.read(buf, sizeof(buf));
+		ret = BIO_write(sink.handle(), buf, stream.gcount());
+		// BIO_write must succeed writing the exact amount of bytes we pass to it.
+		if (ret == 0 || ret == -1)
+			throw std::ios_base::failure("BIO_write failed");
+		else if (ret == -2)
+			throw std::ios_base::failure("BIO_write is not implemented for this BIO");
+	}
+
+	// TODO: Need to check result? 1=success, 0 or -1=failure
+	ret = BIO_flush(sink.handle());
+
+	return stream;
+}
+
+} // namespace sslpkix
