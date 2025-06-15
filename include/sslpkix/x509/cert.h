0562f3066816c36363753f7f8521bc918c27794d
Jardel Weyrich <jweyrich@gmail.com>
2025-06-15 17:12:40 -0300

    Modernize Certificate

--- include/sslpkix/x509/cert.h
+++ include/sslpkix/x509/cert.h
@@ -1,7 +1,9 @@
 #pragma once
 
-//#include <cassert>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <utility>
 #include <openssl/x509v3.h>
 #include "sslpkix/x509/digest.h"
 #include "sslpkix/x509/key.h"
@@ -11,218 +13,323 @@ namespace sslpkix {
 
 class Certificate {
 public:
-	typedef X509 handle_type;
-	struct Version {
-		enum EnumType {
-			v2 = 2,
-			v3 = 3,
-			invalid = -1
-		};
+	using handle_type = X509;
+
+	// Custom deleter for OpenSSL X509
+	struct Deleter {
+		void operator()(X509* ptr) const noexcept {
+			if (ptr) {
+				X509_free(ptr);
+			}
+		}
 	};
+
+    using handle_ptr = std::unique_ptr<X509, Deleter>;
+
+    enum class Version : int {
+        v2 = 2,
+        v3 = 3,
+        invalid = -1
+    };
+
 public:
-	Certificate()
-		: _handle(NULL)
-		, _version(Version::invalid)
-		, _serial(0)
-	{
-	}
-	Certificate(const Certificate& other)
-		: _handle(NULL)
-		, _version(Version::invalid)
-		, _serial(0)
-	{
-		_handle = X509_dup(other.handle());
-		if (_handle == NULL) {
-			// std::cerr << "Failed to copy certificate" << std::endl;
-			throw std::bad_alloc();
-		}
-		reload_data();
-	}
-	Certificate& operator=(Certificate other) {
-		release();
-		swap(*this, other);
-		return *this;
-	}
-	friend void swap(Certificate& a, Certificate& b) { // nothrow
-		using std::swap; // enable ADL
-		swap(a._handle, b._handle);
-		swap(a._version, b._version);
-		swap(a._serial, b._serial);
-		swap(a._pubkey, b._pubkey);
-		swap(a._subject, b._subject);
-		swap(a._issuer, b._issuer);
-	}
-	virtual ~Certificate() {
-		release();
-	}
-	handle_type *handle() const {
-		//assert(_handle != NULL);
-		return _handle;
-	}
-	bool create() {
-		release();
-		_handle = X509_new();
-		if (_handle == NULL) {
-			std::cerr << "Failed to create certificate" << std::endl;
-			return false;
-		}
-		reload_data();
-		return true;
-	}
-	bool set_version(Version::EnumType version) {
-		int ret = X509_set_version(_handle, static_cast<long>(version));
-		if (ret == 0) {
-			std::cerr << "Failed to set version" << std::endl;
-			return false;
-		}
-		_version = version;
-		return true;
-	}
-	Version::EnumType version() const {
-		return _version;
-	}
-	bool set_serial(long serial) {
-		ASN1_INTEGER_set(X509_get_serialNumber(_handle), serial);
-		_serial = serial;
-		return true;
-	}
-	long serial() const {
-		return _serial;
-	}
-	bool set_valid_since(int days) {
-		X509_gmtime_adj(X509_get_notBefore(_handle), (long)60 * 60 * 24 * days);
-		return true;
-	}
-	bool set_valid_until(int days) {
-		X509_gmtime_adj(X509_get_notAfter(_handle), (long)60 * 60 * 24 * days);
-		return true;
-	}
-	bool set_pubkey(Key& key) {
-		int ret = X509_set_pubkey(_handle, key.handle());
-		if (ret == 0) {
-			std::cerr << "Failed to set public key" << std::endl;
-			return false;
-		}
-		_pubkey.set_handle(key.handle());
-		return true;
-	}
-	Key& pubkey() {
-		return _pubkey;
-	}
-	bool sign(PrivateKey& key, Digest::type_e digest = Digest::TYPE_SHA1) {
-		if (!X509_sign(_handle, key.handle(), Digest::handle(digest))) {
-			std::cerr << "Failed to sign" << std::endl;
-			return false;
-		}
-		return true;
-	}
-	bool add_extension(int nid, const char *value) {
-		X509V3_CTX ctx;
-		// This sets the 'context' of the extensions.
-		// No configuration database
-		X509V3_set_ctx_nodb(&ctx);
-		// Issuer and subject certs: both the target since it is self signed,
-		// no request and no CRL
-		X509V3_set_ctx(&ctx, _handle, _handle, NULL, NULL, 0);
-		X509_EXTENSION *ext = X509V3_EXT_conf_nid(NULL, &ctx, nid, (char *)value);
-		if (ext == NULL) {
-			std::cerr << "Failed to add extension: " << nid << std::endl;
-			return false;
-		}
-		X509_add_ext(_handle, ext, -1);
-		X509_EXTENSION_free(ext);
-		return true;
-	}
-	bool add_extension(X509_EXTENSION *ext) {
-		if (ext == NULL)
-			return false;
-		X509V3_CTX ctx;
-		// This sets the 'context' of the extensions.
-		// No configuration database
-		X509V3_set_ctx_nodb(&ctx);
-		// Issuer and subject certs: both the target since it is self signed,
-		// no request and no CRL
-		X509V3_set_ctx(&ctx, _handle, _handle, NULL, NULL, 0);
-		X509_add_ext(_handle, ext, -1);
-		return true;
-	}
-	bool set_subject(CertificateName& subject) {
-		X509_set_subject_name(_handle, subject.handle());
-		_subject.wrap_external(X509_get_subject_name(_handle));
-		return true;
-	}
-	CertificateName& subject() {
-		return _subject;
-	}
-	bool set_issuer(CertificateName& issuer) {
-		X509_set_issuer_name(_handle, issuer.handle());
-		_issuer.wrap_external(X509_get_issuer_name(_handle));
-		return true;
-	}
-	CertificateName& issuer() {
-		return _issuer;
-	}
-	bool verify_signature(Key& key) const {
-		int ret = X509_verify(_handle, key.handle());
-		return ret != 0;
-	}
-	bool check_private_key(PrivateKey& key) const {
-		int ret = X509_check_private_key(_handle, key.handle());
-		return ret != 0;
-	}
-	virtual bool load(IoSink& sink) {
-		release();
-		_handle = PEM_read_bio_X509(sink.handle(), NULL, NULL, NULL);
-		if (_handle == NULL) {
-			std::cerr << "Failed to load certificate: " << sink.source() << std::endl;
-			return false;
-		}
-		reload_data();
-		return true;
-	}
-	virtual bool save(IoSink& sink) const {
-		if (_handle == NULL)
-			return false;
-		if (!X509_print(sink.handle(), _handle) || !PEM_write_bio_X509(sink.handle(), _handle)) {
-			std::cerr << "Failed to save certificate: " << sink.source() << std::endl;
-			return false;
-		}
-		return true;
-	}
-	friend bool operator==(const Certificate& lhs, const Certificate& rhs) {
-		return X509_cmp(lhs._handle, rhs._handle) == 0 &&
-			lhs._subject == rhs._subject &&
-			lhs._issuer == rhs._issuer;
-	}
-	friend bool operator!=(const Certificate& lhs, const Certificate& rhs) {
-		return !(lhs == rhs);
-	}
-protected:
-	void release() {
-		if (_handle != NULL) {
-			X509_free(_handle);
-			_handle = NULL;
-		}
-	}
-	void reload_data() {
-		_version = static_cast<Version::EnumType>(X509_get_version(_handle));
-		_serial = ASN1_INTEGER_get(X509_get_serialNumber(_handle));
-		// TODO(jweyrich): Find a better way to give these warnings to the user.
-		switch (_serial) {
-			//case 0: std::cerr << "The certificate serial is possibly NULL." << std::endl; break;
-			case 0xffffffffL: std::cerr << "The certificate serial is too large to fit in a long." << std::endl; break;
-		}
-		_pubkey.set_handle(X509_get_pubkey(_handle));
-		_subject.wrap_external(X509_get_subject_name(_handle));
-		_issuer.wrap_external(X509_get_issuer_name(_handle));
-	}
-protected:
-	handle_type *_handle;
-	Version::EnumType _version;
-	long _serial;
-	Key _pubkey;
-	CertificateName _subject;
-	CertificateName _issuer;
+    // Default constructor
+    Certificate() = default;
+
+    // Copy constructor
+    Certificate(const Certificate& other) {
+        if (other._handle) {
+            auto* duplicated = X509_dup(other._handle.get());
+            if (!duplicated) {
+                throw std::bad_alloc();
+            }
+            _handle.reset(duplicated);
+            reload_data();
+        }
+    }
+
+    // Move constructor
+    Certificate(Certificate&& other) noexcept = default;
+
+    // Copy assignment
+    Certificate& operator=(const Certificate& other) {
+        if (this != &other) {
+            Certificate temp(other);
+            *this = std::move(temp);
+        }
+        return *this;
+    }
+
+    // Move assignment
+    Certificate& operator=(Certificate&& other) noexcept = default;
+
+    // Destructor
+    ~Certificate() = default;
+
+    // Get raw handle (const version preferred)
+    const handle_type* handle() const noexcept {
+        return _handle.get();
+    }
+
+    // Get raw handle (non-const for OpenSSL functions that need it)
+    handle_type* handle() noexcept {
+        return _handle.get();
+    }
+
+    // Check if certificate is valid/loaded
+    bool is_valid() const noexcept {
+        return _handle != nullptr;
+    }
+
+    // Explicit bool conversion
+    explicit operator bool() const noexcept {
+        return is_valid();
+    }
+
+    // Create new certificate
+    bool create() {
+        auto* new_cert = X509_new();
+        if (!new_cert) {
+            std::cerr << "Failed to create certificate\n";
+            return false;
+        }
+        _handle.reset(new_cert);
+        reload_data();
+        return true;
+    }
+
+    bool set_version(Version version) {
+        if (!_handle) return false;
+
+        int ret = X509_set_version(_handle.get(), static_cast<long>(version));
+        if (ret == 0) {
+            std::cerr << "Failed to set version\n";
+            return false;
+        }
+        _version = version;
+        return true;
+    }
+
+    Version version() const noexcept {
+        return _version;
+    }
+
+    bool set_serial(long serial) {
+        if (!_handle) return false;
+
+        ASN1_INTEGER_set(X509_get_serialNumber(_handle.get()), serial);
+        _serial = serial;
+        return true;
+    }
+
+    long serial() const noexcept {
+        return _serial;
+    }
+
+    bool set_valid_since(int days) {
+        if (!_handle) return false;
+
+        X509_gmtime_adj(X509_get_notBefore(_handle.get()),
+                       static_cast<long>(60) * 60 * 24 * days);
+        return true;
+    }
+
+    bool set_valid_until(int days) {
+        if (!_handle) return false;
+
+        X509_gmtime_adj(X509_get_notAfter(_handle.get()),
+                       static_cast<long>(60) * 60 * 24 * days);
+        return true;
+    }
+
+    bool set_pubkey(const Key& key) {
+        if (!_handle) return false;
+
+        int ret = X509_set_pubkey(_handle.get(), key.handle());
+        if (ret == 0) {
+            std::cerr << "Failed to set public key\n";
+            return false;
+        }
+        _pubkey.set_handle(key.handle());
+        return true;
+    }
+
+    const Key& pubkey() const noexcept {
+        return _pubkey;
+    }
+
+    Key& pubkey() noexcept {
+        return _pubkey;
+    }
+
+    bool sign(const PrivateKey& key, Digest::type_e digest = Digest::TYPE_SHA1) {
+        if (!_handle) return false;
+
+        if (!X509_sign(_handle.get(), key.handle(), Digest::handle(digest))) {
+            std::cerr << "Failed to sign certificate\n";
+            return false;
+        }
+        return true;
+    }
+
+    bool add_extension(int nid, const char* value) {
+        if (!_handle || !value) return false;
+
+        X509V3_CTX ctx;
+        X509V3_set_ctx_nodb(&ctx);
+        X509V3_set_ctx(&ctx, _handle.get(), _handle.get(), nullptr, nullptr, 0);
+
+        auto* ext = X509V3_EXT_conf_nid(nullptr, &ctx, nid, const_cast<char*>(value));
+        if (!ext) {
+            std::cerr << "Failed to create extension: " << nid << '\n';
+            return false;
+        }
+
+        // Use RAII for extension cleanup
+        std::unique_ptr<X509_EXTENSION, decltype(&X509_EXTENSION_free)>
+            ext_guard(ext, &X509_EXTENSION_free);
+
+        if (X509_add_ext(_handle.get(), ext, -1) == 0) {
+            std::cerr << "Failed to add extension: " << nid << '\n';
+            return false;
+        }
+
+        return true;
+    }
+
+    bool add_extension(X509_EXTENSION* ext) {
+        if (!_handle || !ext) return false;
+
+        X509V3_CTX ctx;
+        X509V3_set_ctx_nodb(&ctx);
+        X509V3_set_ctx(&ctx, _handle.get(), _handle.get(), nullptr, nullptr, 0);
+
+        return X509_add_ext(_handle.get(), ext, -1) != 0;
+    }
+
+    bool set_subject(const CertificateName& subject) {
+        if (!_handle) return false;
+
+        if (X509_set_subject_name(_handle.get(), subject.handle()) == 0) {
+            return false;
+        }
+        _subject.wrap_external(X509_get_subject_name(_handle.get()));
+        return true;
+    }
+
+    const CertificateName& subject() const noexcept {
+        return _subject;
+    }
+
+    CertificateName& subject() noexcept {
+        return _subject;
+    }
+
+    bool set_issuer(const CertificateName& issuer) {
+        if (!_handle) return false;
+
+        if (X509_set_issuer_name(_handle.get(), issuer.handle()) == 0) {
+            return false;
+        }
+        _issuer.wrap_external(X509_get_issuer_name(_handle.get()));
+        return true;
+    }
+
+    const CertificateName& issuer() const noexcept {
+        return _issuer;
+    }
+
+    CertificateName& issuer() noexcept {
+        return _issuer;
+    }
+
+    bool verify_signature(const Key& key) const {
+        if (!_handle) return false;
+        return X509_verify(_handle.get(), key.handle()) != 0;
+    }
+
+    bool check_private_key(const PrivateKey& key) const {
+        if (!_handle) return false;
+        return X509_check_private_key(_handle.get(), key.handle()) != 0;
+    }
+
+    virtual bool load(IoSink& sink) {
+        auto* cert = PEM_read_bio_X509(sink.handle(), nullptr, nullptr, nullptr);
+        if (!cert) {
+            std::cerr << "Failed to load certificate: " << sink.source() << '\n';
+            return false;
+        }
+
+        _handle.reset(cert);
+        reload_data();
+        return true;
+    }
+
+    virtual bool save(const IoSink& sink) const {
+        if (!_handle) return false;
+
+        if (!X509_print(sink.handle(), _handle.get()) ||
+            !PEM_write_bio_X509(sink.handle(), _handle.get())) {
+            std::cerr << "Failed to save certificate: " << sink.source() << '\n';
+            return false;
+        }
+        return true;
+    }
+
+    // Equality operators
+    friend bool operator==(const Certificate& lhs, const Certificate& rhs) {
+        // Handle null cases
+        if (!lhs._handle && !rhs._handle) return true;
+        if (!lhs._handle || !rhs._handle) return false;
+
+        return X509_cmp(lhs._handle.get(), rhs._handle.get()) == 0 &&
+               lhs._subject == rhs._subject &&
+               lhs._issuer == rhs._issuer;
+    }
+
+    friend bool operator!=(const Certificate& lhs, const Certificate& rhs) {
+        return !(lhs == rhs);
+    }
+
+    // Swap function for copy-and-swap idiom
+    friend void swap(Certificate& a, Certificate& b) noexcept {
+        using std::swap;
+        swap(a._handle, b._handle);
+        swap(a._version, b._version);
+        swap(a._serial, b._serial);
+        swap(a._pubkey, b._pubkey);
+        swap(a._subject, b._subject);
+        swap(a._issuer, b._issuer);
+    }
+
+private:
+    void reload_data() {
+        if (!_handle) {
+            _version = Version::invalid;
+            _serial = 0;
+            return;
+        }
+
+        _version = static_cast<Version>(X509_get_version(_handle.get()));
+        _serial = ASN1_INTEGER_get(X509_get_serialNumber(_handle.get()));
+
+        // Handle potential overflow warning
+        if (_serial == 0xffffffffL) {
+            std::cerr << "Warning: Certificate serial number is too large to fit in a long\n";
+        }
+
+        _pubkey.set_handle(X509_get_pubkey(_handle.get()));
+        _subject.wrap_external(X509_get_subject_name(_handle.get()));
+        _issuer.wrap_external(X509_get_issuer_name(_handle.get()));
+    }
+
+private:
+    handle_ptr _handle;
+    Version _version{Version::invalid};
+    long _serial{0};
+    Key _pubkey;
+    CertificateName _subject;
+    CertificateName _issuer;
 };
 
 } // namespace sslpkix
