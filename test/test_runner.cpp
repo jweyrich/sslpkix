765d1075ae803e7607ad5b3b4a9efead1c115f5d
Jardel Weyrich <jweyrich@gmail.com>
2025-06-25 01:11:02 -0300

    Remove unintended output from test runner

--- test/test_runner.cpp
+++ test/test_runner.cpp
@@ -10,19 +10,16 @@ struct TestRunnerSetup {
             std::cerr << "ERROR: Failed to initialize SSLPKIX" << std::endl;
             exit(EXIT_FAILURE);
         }
-        std::cout << "SSLPKIX initialized" << std::endl;
 
         success = sslpkix::seed_prng();
         if (!success) {
             std::cerr << "ERROR: Failed to seed the PRNG" << std::endl;
             exit(EXIT_FAILURE);
         }
-        std::cout << "PRNG seeded" << std::endl;
     }
 
     ~TestRunnerSetup() {
         sslpkix::shutdown();
-        std::cout << "SSLPKIX shutdown" << std::endl;
     }
 };
 
